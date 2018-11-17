3b106a570ade2559e307576b8bf42d4c23fa3045
ffrancajordao <42717180+ffrancajordao@users.noreply.github.com>
2018-11-17 13:19:16 -0500

    Add files via upload

--- Prescriptions.java
+++ Prescriptions.java
@@ -1,24 +1,24 @@
 public class Prescriptions {
 	
-	private String pre_ID;
+	private int pre_ID;
 	private String date;
 	private String pharmacist;
 
 	public Prescriptions() {
- 		pre_ID = "";
+ 		pre_ID = 0;
 		pharmacist = "";		
 	}
 	
-	public Prescriptions(String u_ID, String u_Phar){
+	public Prescriptions(int u_ID, String u_Phar){
 		pre_ID = u_ID;
 		pharmacist = u_Phar;
 	}
 
-	public String getPre_ID() {
+	public int getPre_ID() {
 		return pre_ID;
 	}
 
-	public void setPre_ID(String u_preID) {
+	public void setPre_ID(int u_preID) {
 		pre_ID = u_preID;
 	}
 
